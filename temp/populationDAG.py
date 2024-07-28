fa1ec406dda8f12baf8ccb1f13efc14a101687e1
Elisha0510 <sjeong35@knu.ac.kr>
2024-07-28 16:19:57 +0900

    refactor: csv 파일 이름을 매월 다르도록 설정

--- temp/populationDAG.py
+++ temp/populationDAG.py
@@ -74,31 +74,41 @@ def extract(**context):
     
     driver.switch_to.frame(iframe)
 
-    button_1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ico_download"]/a')))
-    button_1.click()
+    time.sleep(3)
 
-    button_2 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="csvradio"]')))
+    button_1 = driver.find_element(By.XPATH, '//*[@id="ico_download"]/a')
+    button_1.click()
+    logging.info("Button clicked successfully!")
+
+    time.sleep(5)
+
+    button_2 = driver.find_element(By.XPATH, '//*[@id="csvradio"]')
     button_2.click()
+    time.sleep(2)
+    logging.info("Button clicked successfully!_2")
 
     button_3 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="pop_downgrid2"]/div[2]/div[3]/span[1]/a')))
     button_3.click()
 
     time.sleep(10)
 
+    year = datetime.now().strftime('%Y')
+    month =  int(datetime.now().strftime('%m'))
+    download_file_name = f"등록인구(월별)_{year}0{month}*"
+    data_file_name = f"seoul_population_{year}0{month-1}"
+
     download_dir = '/opt/airflow/downloads'
-    files = glob.glob(os.path.join(download_dir, "등록인구(월별)_202407*"))
+    files = glob.glob(os.path.join(download_dir, download_file_name))
     if files:
         latest_file = max(files, key=os.path.getctime)
-        new_name = os.path.join(download_dir, "등록인구_202406.csv")
+        new_name = os.path.join(download_dir, data_file_name)
         os.rename(latest_file, new_name)
         logging.info(f"File renamed to: {new_name}")
     else:
         logging.info("No files found for renaming")
 
-
-    file_name = '등록인구_202406.csv'
     
-    file_path = f'/opt/airflow/downloads/{file_name}'
+    file_path = f'/opt/airflow/downloads/{data_file_name}'
     
     file = Path(file_path)
     if file.is_file():
@@ -108,7 +118,7 @@ def extract(**context):
 
     driver.quit() 
     logging.info("Extract done")
-    return file_name
+    return data_file_name
 
 # 다운받은 CSV 파일 변환하는 함수
 def transform(**context):
@@ -129,24 +139,25 @@ def transform(**context):
         df = df.drop('합계', axis=1)
         df.to_csv(csv_file_path, index=False, header=False)
         logging.info("transform finished")
-        return csv_file_path
+        return csv_file_name
         
     except Exception as e:
         print(f"An error occurred: {e}")
 
 # 변환한 CSV 파일 S3에 적재하는 함수
-def upload_to_S3(file_path, **kwargs):
+def upload_to_S3(name, **kwargs):
     bucket_name = 'team-ariel-2-data'
     hook = S3Hook(aws_conn_id='S3_conn')
     hook.load_file(
-        filename=file_path,
-        key='data/등록인구(월별)_202406.csv', 
-        bucket_name= bucket_name, 
+        filename=f'downloads/{name}',
+        key=f'data/{name}.csv', 
+        bucket_name=bucket_name, 
         replace=True
     )
+    return name
 
 # S3에서 Redshift로 COPY해서 적재하는 함수
-def load_to_redshift():
+def load_to_redshift(name, **kwargs):
     redshift_hook = PostgresHook(postgres_conn_id='rs_conn')
     conn = redshift_hook.get_conn()
     cursor = conn.cursor()
@@ -154,7 +165,7 @@ def load_to_redshift():
     # Redshift용 COPY 명령문
     copy_query = f"""
     COPY raw_data.seoul_population
-    FROM 's3://team-ariel-2-data/data/등록인구(월별)_202406.csv'
+    FROM 's3://team-ariel-2-data/data/{name}'
     IAM_ROLE 'arn:aws:iam::862327261051:role/service-role/AmazonRedshift-CommandsAccessRole-20240716T180249'
     CSV
     """
@@ -188,13 +199,16 @@ populationDataTransform = PythonOperator(
 upload_data_to_S3 = PythonOperator(
     task_id = "upload_to_S3",
     python_callable=upload_to_S3,
-    op_kwargs={'file_path': '{{ task_instance.xcom_pull(task_ids="population_transform") }}'}
+    op_kwargs={'name': '{{ task_instance.xcom_pull(task_ids="population_transform") }}'},
+    dag=dag
 )
 
 # S3에서 Redshift로 COPY해서 적재하는 Task
 load_data_to_redshift = PythonOperator(
     task_id = "load_to_redshift",
-    python_callable=load_to_redshift
+    python_callable=load_to_redshift,
+    op_kwargs={'name': '{{ task_instance.xcom_pull(task_ids="upload_to_S3") }}'},
+    dag=dag
 )
 
 createPopulationTable >> populationDataExtract >> populationDataTransform >> upload_data_to_S3 >> load_data_to_redshift
