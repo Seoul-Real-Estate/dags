2d2deba760263fec9e7e5f2871949f9702445b20
Elisha0510 <sjeong35@knu.ac.kr>
2024-07-30 16:43:37 +0900

    refactor: 다운로드 경로 상대 경로로 수정

--- dags/download_test_dag.py
+++ dags/download_test_dag.py
@@ -1,16 +1,23 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import time
+from pathlib import Path
 import logging
+import pandas as pd
 
 from datetime import datetime
 import logging
 import pendulum
+import os
+import glob
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -36,7 +43,7 @@ def extract(**context):
     chrome_options.add_argument("--headless")  # Headless 모드 설정
     chrome_options.add_argument("--download.default_directory=/downloads")
     chrome_options.add_experimental_option("prefs", {
-        "download.default_directory": "/home/ubuntu/airflow/downloads",
+        "download.default_directory": "../downloads",
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
@@ -52,6 +59,27 @@ def extract(**context):
     print("Button clicked successfully!")
     
     time.sleep(10)
+
+    download_dir = '../downloads'
+    files = glob.glob(os.path.join(download_dir, "서울시 시내버스 정류소 현황_*"))
+    if files:
+        latest_file = max(files, key=os.path.getctime)
+        new_name = os.path.join(download_dir, "seoul_bus_station.xlsx")
+        os.rename(latest_file, new_name)
+        logging.info(f"File renamed to: {new_name}")
+    else:
+        logging.info("No files found for renaming")
+
+
+    file_name = 'seoul_bus_station.xlsx'
+    
+    file_path = f'../downloads/{file_name}'
+    
+    file = Path(file_path)
+    if file.is_file():
+        logging.info("Download Succeed")
+    else:
+        logging.info("Download Failed")
 
 # 서울시 버스 정보 csv 파일로 다운받는 Task
 busDataExtract = PythonOperator(
