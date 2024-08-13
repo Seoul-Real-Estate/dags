c13914710a777849dd9da850b8ac22a9573cca08
jki5410 <166671365+jki5410@users.noreply.github.com>
2024-08-14 02:42:31 +0900

    fix: redshift 연결 함수 및 동 데이터 수집 함수 버그 수정

--- dags/seoul_park.py
+++ dags/seoul_park.py
@@ -7,29 +7,29 @@ import requests
 import psycopg2
 import logging
 
-SCHEMA_RAW = "raw_data"
-SCHEMA_ANALYTICS = "analytics"
+RAW_SCHEMA = "raw_data"
 TEMP_TABLE = "temp_seoul_park"
 MAIN_TABLE = "seoul_park"
 
 SEOUL_API_KEY = Variable.get("seoul_api_key")
 SEOUL_API_BASE_URL = "http://openapi.seoul.go.kr:8088"
-SEOUL_API_RESPONSE_TYPE = "json/"
+SEOUL_API_RESPONSE_TYPE = "json"
 SEOUL_API_ENDPOINT = "SearchParkInfoService"
 
-API_KAKAO_URL = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json"
-API_KAKAO_KEY = Variable.get("kakao_api_key")
+KAKAO_COORD_TO_REGION_URL = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json"
+KAKAO_API_KEY = Variable.get("kakao_api_key")
 
 # Redshift 연결 함수
-def get_redshift_connection():
+def get_redshift_connection(autocommit=True):
     hook = PostgresHook(postgres_conn_id="redshift_dev")
     conn = hook.get_conn()
+    conn.autocommit = autocommit
     return conn
 
 
-# 쿼리 실행 함수
-def execute_query(query, parameters=None, fetchall=False, executemany=False, autocommit=True):
-    conn = get_redshift_connection()
+# 쿼리문 실행시키는 함수
+def execute_query(query, parameters=None, autocommit=True, fetchall=False, executemany=False):
+    conn = get_redshift_connection(autocommit)
     cur = conn.cursor()
     try:
         if not autocommit:
@@ -48,10 +48,13 @@ def execute_query(query, parameters=None, fetchall=False, executemany=False, aut
         
         if fetchall:
             return cur.fetchall()
-    except (Exception, psycopg2.DatabaseError) as error:
-        logging.error(f"Query error: {error}")
-        if not autocommit:
-            cur.execute("ROLLBACK;")
+    except psycopg2.DatabaseError as db_error:
+        logging.error(f"Database error: {db_error}")
+        cur.execute("ROLLBACK;")
+        raise
+    except Exception as error:
+        logging.error(f"execute query error: {error}")
+        cur.execute("ROLLBACK;")
         raise
     finally:
         cur.close()
@@ -60,7 +63,7 @@ def execute_query(query, parameters=None, fetchall=False, executemany=False, aut
 
 # 전체 데이터의 개수를 반환하는 함수
 def get_list_total_count():
-    url = f"{SEOUL_API_BASE_URL}{SEOUL_API_KEY}/{SEOUL_API_ENDPOINT}/1/1"
+    url = f"{SEOUL_API_BASE_URL}/{SEOUL_API_KEY}/{SEOUL_API_RESPONSE_TYPE}/{SEOUL_API_ENDPOINT}/1/1"
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -70,6 +73,53 @@ def get_list_total_count():
         logging.error(f"API request failed: {error}")
         raise
 
+
+# 영화상영관 데이터를 API에서 가져오는 함수
+def fetch_seoul_data(start_index, end_index):
+    url = f"{SEOUL_API_BASE_URL}/{SEOUL_API_KEY}/{SEOUL_API_RESPONSE_TYPE}/{SEOUL_API_ENDPOINT}/{start_index}/{end_index}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()[SEOUL_API_ENDPOINT]["row"]
+    except Exception as error:
+        logging.error(f"failed fetch seoul data: {error}")
+        raise
+
+
+# 데이터 변환
+def transform_data(data_list):
+    records = []
+    for data in data_list:
+        record = (
+            data["P_PARK"],
+            data["P_ZONE"],
+            data["P_ADDR"],
+            data["LATITUDE"],
+            data["LONGITUDE"]
+        )
+        records.append(record)
+    return records
+
+
+# 좌표로 법정동 정보를 가져오는 함수
+def get_dong_name(x, y):
+    if not x or not y:
+        return None
+    
+    headers = {"Authorization": f"KakaoAK {KAKAO_API_KEY}"}
+    params = {"x": x, "y": y}
+    try:
+        response = requests.get(KAKAO_COORD_TO_REGION_URL, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+        legal_dong_name = data["documents"][0]["region_3depth_name"]
+    except (IndexError, KeyError, TypeError, AttributeError) as e:
+        logging.error(f"Error processing dong name response for coordinates ({x}, {y}): {e}")
+        legal_dong_name = None
+    except Exception as e:
+        logging.error(f"Failed to get dong name for coordinates ({x}, {y}): {e}")
+        raise
+    return legal_dong_name
 
 # DAG 정의
 @dag(
@@ -88,8 +138,8 @@ def seoul_park():
     @task
     def create_temp_table():
         create_query = f"""
-        DROP TABLE IF EXISTS {SCHEMA_ANALYTICS}.{TEMP_TABLE};
-        CREATE TABLE {SCHEMA_ANALYTICS}.{TEMP_TABLE} (
+        DROP TABLE IF EXISTS {RAW_SCHEMA}.{TEMP_TABLE};
+        CREATE TABLE {RAW_SCHEMA}.{TEMP_TABLE} (
             id INT IDENTITY(1, 1) PRIMARY KEY,
             park_name VARCHAR(100),
             district_name VARCHAR(50),
@@ -105,85 +155,63 @@ def seoul_park():
     # 데이터 수집 task
     @task
     def process_data():
+        total_count = get_list_total_count()
         start_index = 1
         unit_value = 1000
-        records = []
-
-        total_count = get_list_total_count()
+        all_records = []
 
         while start_index <= total_count:
             end_index = min(start_index + unit_value - 1, total_count)
-            url = f"{SEOUL_API_BASE_URL}{SEOUL_API_KEY}/{SEOUL_API_ENDPOINT}/{start_index}/{end_index}"
-            try:
-                response = requests.get(url)
-                response.raise_for_status()
-                data_list = response.json()[SEOUL_API_ENDPOINT]["row"]
+            data_list = fetch_seoul_data(start_index, end_index)
+            records = transform_data(data_list)
+            all_records.extend(records)
+            start_index += unit_value
+        logging.info(f"process_raw_data {len(all_records)} records.")
 
-                for data in data_list:
-                    record = (
-                        data["P_PARK"],
-                        data["P_ZONE"],
-                        data["P_ADDR"],
-                        data["LATITUDE"],
-                        data["LONGITUDE"]
-                    )
-                    records.append(record)
-
-                start_index += unit_value
-            except Exception as error:
-                logging.error(f"process data failed: {error}")
-                raise
-
-        return records
-
+        return all_records
+    
 
     # 법정동 데이터 추가하는 task
     @task
     def add_dong_data(records):
         geocoded_records = []
-        url_template = API_KAKAO_URL.format
-        headers = {"Authorization": f"KakaoAK {API_KAKAO_KEY}"}
-
         for record in records:
             latitude, longitude = record[3], record[4]
-            url = url_template(longitude=longitude, latitude=latitude)
 
-            try:
-                response = requests.get(url, headers=headers)
-                response.raise_for_status()
-                data = response.json()
-                dong = data["documents"][0].get("region_3depth_name", None)
-            except (IndexError, KeyError, TypeError, AttributeError) as error:
-                logging.error(f"Failed to get dong name for coordinates ({longitude}, {latitude}): {error}")
-                dong = None
-            except Exception as error:
-                logging.error(f"API request faile : {error}")
-                raise
-
-            geocoded_record = list(record) + [dong]
+            legal_dong_name = get_dong_name(longitude, latitude)
+            
+            geocoded_record = list(record) + [legal_dong_name]
             geocoded_records.append(tuple(geocoded_record))
-
+        logging.info(f"Geocoded {len(geocoded_records)} records.")
         return geocoded_records
+    
 
     # 테이블에 데이터 적재하는 task
     @task
     def load_temp_table(records):
         insert_query = f"""
-        INSERT INTO {SCHEMA_ANALYTICS}.{TEMP_TABLE} (
+        INSERT INTO {RAW_SCHEMA}.{TEMP_TABLE} (
             park_name, district_name, address, latitude, longitude, legal_dong_name
         ) VALUES (%s, %s, %s, %s, %s, %s)
         """
         execute_query(insert_query, parameters=records, executemany=True, autocommit=False)
 
+
     # 테이블 이름 변경하는 task
     @task
     def rename_tables():
         rename_query = f"""
-        DROP TABLE IF EXISTS {SCHEMA_ANALYTICS}.{MAIN_TABLE};
-        ALTER TABLE {SCHEMA_ANALYTICS}.{TEMP_TABLE} RENAME TO {MAIN_TABLE};
+        DROP TABLE IF EXISTS {RAW_SCHEMA}.{MAIN_TABLE};
+        ALTER TABLE {RAW_SCHEMA}.{TEMP_TABLE} RENAME TO {MAIN_TABLE};
         """
         execute_query(rename_query, autocommit=False)
 
-    create_temp_table() >> process_data() >> add_dong_data() >> load_temp_table() >> rename_tables()
+    create_temp_table_task = create_temp_table()
+    process_data_task = process_data()
+    add_dong_data_task = add_dong_data(process_data_task)
+    load_temp_table_task = load_temp_table(add_dong_data_task)
+    rename_tables_task = rename_tables()
+
+    create_temp_table_task >> process_data_task >> add_dong_data_task >> load_temp_table_task >> rename_tables_task
 
 seoul_park()
