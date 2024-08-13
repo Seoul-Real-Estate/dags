aaf7b708be96b4ae0ea3001cd7cd13d49000c62f
jki5410 <166671365+jki5410@users.noreply.github.com>
2024-08-13 16:39:53 +0900

    feat: analytics 테이블 생성하는 task 추가 및 코드 리팩토링

--- dags/seoul_school.py
+++ dags/seoul_school.py
@@ -1,186 +1,278 @@
-from airflow import DAG
-from airflow.decorators import task
+from airflow.decorators import task, dag
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import Variable
 from datetime import datetime, timedelta
 import requests
 import psycopg2
-import time
 import logging
+
+RAW_SCHEMA = "raw_data"
+ANALYTICS_SCHEMA = "analytics"
+TEMP_TABLE = "temp_seoul_school"
+MAIN_TABLE = "seoul_school"
+
+SEOUL_API_KEY = Variable.get("seoul_api_key")
+BASE_URL = "http://openapi.seoul.go.kr:8088/"
+ENDPOINT = "/json/neisSchoolInfo"
+
+KAKAO_API_KEY = Variable.get("kakao_api_key")
+KAKAO_GEOCODE_URL = "https://dapi.kakao.com/v2/local/search/address.json"
+
 
 # Redshift 연결 함수
 def get_redshift_connection(autocommit=True):
-    hook = PostgresHook(postgres_conn_id='redshift_dev')
+    hook = PostgresHook(postgres_conn_id="redshift_dev")
     conn = hook.get_conn()
     conn.autocommit = autocommit
-    return conn.cursor()
+    return conn
 
-# 임시 테이블 생성 task
-@task
-def create_temp_table(schema, temp_table):
-    cur = get_redshift_connection()
-    create_query = f"""
-    DROP TABLE IF EXISTS {schema}.{temp_table};
 
-    CREATE TABLE {schema}.{temp_table} (
-        id INT IDENTITY(1, 1) PRIMARY KEY,
-        school_code VARCHAR(7),
-        school_name VARCHAR(100),
-        school_type VARCHAR(50),
-        postal_code VARCHAR(6),
-        road_address VARCHAR(150),
-        detailed_road_address VARCHAR(150),
-        load_date VARCHAR(8),
-        latitude FLOAT,
-        longitude FLOAT,
-        district_name VARCHAR(30),
-        legal_dong_name VARCHAR(30)
-    );
-    """
-    cur.execute(create_query)
+# 쿼리 실행 함수
+def execute_query(query, parameters=None, fetchall=False, executemany=False):
+    conn = get_redshift_connection(autocommit=False)
+    cur = conn.cursor()
+    try:
+        cur.execute("BEGIN;")
+        if parameters:
+            if executemany:
+                cur.executemany(query, parameters)
+            else:
+                cur.execute(query, parameters)
+        else:
+            cur.execute(query)
+        
+        if fetchall:
+            results = cur.fetchall()
+        cur.execute("COMMIT;")
+        if fetchall:
+            return results
+    except (psycopg2.DatabaseError, Exception) as error:
+        logging.error(f"Error during query execution: {error}")
+        cur.execute("ROLLBACK;")
+        raise
+    finally:
+        cur.close()
+        conn.close()
 
-# API 요청에 대한 전체 데이터의 개수를 반환하는 함수
-def get_list_total_count():
-    api_key = Variable.get('seoul_api_key')
-    url = f"http://openapi.seoul.go.kr:8088/{api_key}/json/neisSchoolInfo/1/1"
+
+# 전체 데이터 개수를 반환하는 함수
+def get_total_data_count():
+    url = f"{BASE_URL}{SEOUL_API_KEY}{ENDPOINT}/1/1"
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        total_count = data["neisSchoolInfo"]["list_total_count"]
-        return total_count
-    
-    except requests.RequestException as error:
+        return data["neisSchoolInfo"]["list_total_count"]
+    except Exception as error:
         logging.error(f"API request failed: {error}")
         raise
 
-# 데이터 수집 task
-@task
-def process_data():
-    api_key = Variable.get('seoul_api_key')
-    start_index = 1
-    unit_value = 1000  # 한 번에 1000개까지 가능
+
+# 학교 데이터를 API에서 가져오는 함수
+def fetch_data(start_index, end_index):
+    url = f"{BASE_URL}{SEOUL_API_KEY}{ENDPOINT}/{start_index}/{end_index}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json().get("neisSchoolInfo", {}).get("row", [])
+    except Exception as error:
+        logging.error(f"API request failed for range {start_index}-{end_index}: {error}")
+        raise
+
+
+# 데이터를 변환하는 함수
+def transform_data(data_list):
     records = []
-
-    total_count = get_list_total_count()
-
-    # unit_value 단위로 나누어 api 요청
-    while start_index <= total_count:
-        end_index = min(start_index + unit_value - 1, total_count)
-        url = f"http://openapi.seoul.go.kr:8088/{api_key}/json/neisSchoolInfo/{start_index}/{end_index}"
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            data_list = response.json()["neisSchoolInfo"]["row"]
-            
-            for data in data_list:
-                record = (
-                    data['SD_SCHUL_CODE'],
-                    data['SCHUL_NM'],
-                    data['SCHUL_KND_SC_NM'],
-                    data['ORG_RDNZC'],
-                    data['ORG_RDNMA'],
-                    data['ORG_RDNDA'],
-                    data['LOAD_DTM']
-                )
-                records.append(record)
-            
-            start_index += unit_value
-        except requests.RequestException as error:
-            logging.error(f"API request failed: {error}")
-            raise
+    for data in data_list:
+        record = (
+            data["SD_SCHUL_CODE"],
+            data["SCHUL_NM"],
+            data["SCHUL_KND_SC_NM"],
+            data["ORG_RDNZC"],
+            data["ORG_RDNMA"],
+            data["ORG_RDNDA"],
+            data["LOAD_DTM"]
+        )
+        records.append(record)
 
     return records
 
-# 위/경도 좌표값 추가하는 task
-@task
-def geocode(records):
-    geocoded_records = []
-    kakao_api_key = Variable.get('kakao_api_key')
 
-    for record in records:
-        road_address = record[4]  # 주소 데이터 'ORG_RDNMA'
-        url = f'https://dapi.kakao.com/v2/local/search/address.json?query={road_address}&analyze_type=exact'
-        headers = {"Authorization": kakao_api_key}
-        try:
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()
-            # 주소 정보에서 좌표 정보 추출
-            documents = response.json().get('documents', [])
-            if documents:
-                latitude = round(float(documents[0].get('y', 0)), 6)  # 소수점 이하 6자리로 반올림
-                longitude = round(float(documents[0].get('x', 0)), 6)  # 소수점 이하 6자리로 반올림
-                district_name = documents[0]['road_address']['region_2depth_name']  # 자치구 이름 
-                legal_dong_name = documents[0]['road_address']['region_3depth_name']  # 법정동 이름
-            else:
-                latitude = None
-                longitude = None
-                district_name = None
-                legal_dong_name = None
-            
-            geocoded_record = list(record) + [latitude, longitude, district_name, legal_dong_name]
-            geocoded_records.append(tuple(geocoded_record))
-
-        except  requests.RequestException as error:
-            logging.error(f"Error geocoding: {error}")
-            raise
-
-        time.sleep(0.1)  
-
-    return geocoded_records
-
-# 임시 테이블에 데이터를 적재하는 task
-@task
-def load_temp_table(schema, temp_table, records):
-    cur = get_redshift_connection(False)
+# 주소로 좌표 정보를 가져오는 함수
+def geocode_address(address):
+    headers = {"Authorization": f"KakaoAK {KAKAO_API_KEY}"}
+    params = {"query": address}
     try:
-        cur.execute("BEGIN;")
+        response = requests.get(KAKAO_GEOCODE_URL, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+        try:
+            latitude = float(data["documents"][0]["y"])
+            longitude = float(data["documents"][0]["x"])
+            district_name = data["documents"][0]["road_address"]["region_2depth_name"]  
+            legal_dong_name = data["documents"][0]["road_address"]["region_3depth_name"]  
+        except (IndexError, KeyError, TypeError, AttributeError) as e:
+            logging.error(f"Error processing geocode response address {address}: {e}")
+            latitude, longitude, district_name, legal_dong_name = None, None, None, None
+    except Exception as e:
+        logging.error(f"Failed to geocode address {address}: {e}")
+        raise
+    return latitude, longitude, district_name, legal_dong_name
+
+
+# DAG 정의
+@dag(
+    start_date=datetime(2024, 7, 10),
+    schedule_interval="0 10 * * 6",  # 토요일 오전 10시 
+    catchup=False,
+    tags=["analytics", "school", "infra", "raw_data"],
+    default_args={
+        "owner": "kain",
+        "retries": 2,
+        "retry_delay": timedelta(minutes=5),
+    }
+)
+def seoul_school():
+
+    # RAW 스키마에 테이블 생성
+    @task
+    def create_raw_table():
+        create_query = f"""
+        DROP TABLE IF EXISTS {RAW_SCHEMA}.{TEMP_TABLE};
+        CREATE TABLE {RAW_SCHEMA}.{TEMP_TABLE} (
+            id INT IDENTITY(1, 1) PRIMARY KEY,
+            school_code VARCHAR(7),
+            school_name VARCHAR(100),
+            school_type VARCHAR(50),
+            postal_code VARCHAR(6),
+            road_address VARCHAR(150),
+            detailed_road_address VARCHAR(150),
+            load_date VARCHAR(8)
+        );
+        """
+        execute_query(create_query)
+
+
+    # 데이터 수집 
+    @task
+    def process_data():
+        start_index = 1
+        unit_value = 1000
+        total_count = get_total_data_count()
+        all_records = []
+        
+        while start_index <= total_count:
+            end_index = min(start_index + unit_value - 1, total_count)
+            data_list = fetch_data(start_index, end_index)
+            records = transform_data(data_list)
+            all_records.extend(records)
+            start_index += unit_value
+        
+        return all_records
+
+
+    # 데이터 적재
+    @task
+    def load_raw_table(records):
         insert_query = f"""
-        INSERT INTO {schema}.{temp_table} (
+        INSERT INTO {RAW_SCHEMA}.{TEMP_TABLE} (
             school_code, school_name, school_type, postal_code, 
-            road_address, detailed_road_address, load_date, latitude, longitude, district_name, legal_dong_name
+            road_address, detailed_road_address, load_date
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        execute_query(insert_query, parameters=records, executemany=True)
+
+
+    # 테이블 이름 변경 
+    @task
+    def rename_raw_table():
+        rename_query = f"""
+        DROP TABLE IF EXISTS {RAW_SCHEMA}.{MAIN_TABLE};
+        ALTER TABLE {RAW_SCHEMA}.{TEMP_TABLE} RENAME TO {MAIN_TABLE};
+        """
+        execute_query(rename_query)
+
+
+    # ANALYTICS 스키마에 테이블 생성
+    @task
+    def create_analytics_table():
+        create_query = f"""
+        DROP TABLE IF EXISTS {ANALYTICS_SCHEMA}.{TEMP_TABLE};
+
+        CREATE TABLE {ANALYTICS_SCHEMA}.{TEMP_TABLE} (
+            id INT IDENTITY(1, 1),
+            school_code VARCHAR(7),
+            school_name VARCHAR(100),
+            school_type VARCHAR(50),
+            postal_code VARCHAR(6),
+            road_address VARCHAR(150),
+            detailed_road_address VARCHAR(150),
+            load_date VARCHAR(8),
+            district_name VARCHAR(30),
+            legal_dong_name VARCHAR(30),
+            latitude float,
+            longitude float
+        );
+        """
+        execute_query(create_query)
+
+
+    # ANALYTICS 스키마에 데이터 삽입
+    @task
+    def populate_analytics_table():
+        # raw_data의 중복 데이터 제거 
+        select_query = f"""
+        SELECT DISTINCT school_code, school_name, school_type, postal_code, 
+                        road_address, detailed_road_address, load_date
+        FROM {RAW_SCHEMA}.{MAIN_TABLE}
+        """
+
+        rows = execute_query(select_query, fetchall=True)
+        analytics_records = []
+
+        for row in rows:
+            school_code, school_name, school_type, postal_code, road_address, detailed_road_address, load_date = row
+
+            # 위/경도 좌표값, 구/동 이름 추가
+            latitude, longitude, district_name, legal_dong_name = geocode_address(road_address)
+
+            analytics_record = (
+                school_code, school_name, school_type, postal_code, 
+                road_address, detailed_road_address, load_date, 
+                district_name, legal_dong_name, latitude, longitude
+            )
+            analytics_records.append(analytics_record)
+
+        insert_query = f"""
+        INSERT INTO {ANALYTICS_SCHEMA}.{TEMP_TABLE} (
+            school_code, school_name, school_type, postal_code, 
+            road_address, detailed_road_address, load_date, 
+            district_name, legal_dong_name, latitude, longitude
         ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
-        for record in records:
-            cur.execute(insert_query, record)
-        cur.execute("COMMIT;")
-    except (Exception, psycopg2.DatabaseError) as error:
-        logging.error(error)
-        cur.execute("ROLLBACK;")
-        raise
+        execute_query(insert_query, parameters=analytics_records, executemany=True)
 
-# 임시 테이블을 원본 테이블로 변경하는 task
-@task
-def swap_tables(schema, temp_table, main_table):
-    cur = get_redshift_connection(False)
-    try:
-        cur.execute("BEGIN;")
-        cur.execute(f"DROP TABLE IF EXISTS {schema}.{main_table};")
-        cur.execute(f"ALTER TABLE {schema}.{temp_table} RENAME TO {main_table};")
-        cur.execute("COMMIT;")
-    except (Exception, psycopg2.DatabaseError) as error:
-        logging.error(error)
-        cur.execute("ROLLBACK;")
-        raise
 
-with DAG(
-    dag_id='seoul_school_v2',
-    start_date=datetime(2024, 7, 10),  
-        schedule_interval='0 17 * * 5',  # 한국 기준 토요일 새벽 2시
-    catchup=False,
-    default_args={
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
-    }
-) as dag:
-    schema = 'raw_data'
-    temp_table = 'temp_seoul_school'
-    main_table = 'seoul_school'
+    # 테이블 이름 변경 
+    @task
+    def rename_analytics_table():
+        rename_query = f"""
+        DROP TABLE IF EXISTS {ANALYTICS_SCHEMA}.{MAIN_TABLE};
+        ALTER TABLE {ANALYTICS_SCHEMA}.{TEMP_TABLE} RENAME TO {MAIN_TABLE};
+        """
+        execute_query(rename_query)
 
-    create_temp_table_task = create_temp_table(schema, temp_table)
-    process_task = process_data()
-    geocode_task = geocode(process_task)
-    load_temp_table_task = load_temp_table(schema, temp_table, geocode_task)
-    swap_tables_task = swap_tables(schema, temp_table, main_table)
 
-    create_temp_table_task >> process_task >> geocode_task >> load_temp_table_task >> swap_tables_task
+    # DAG 태스크 연결
+    create_raw_table_task = create_raw_table()
+    process_data_task = process_data()
+    load_raw_table_task = load_raw_table(process_data_task)
+    rename_raw_table_task = rename_raw_table()
+    create_analytics_table_task = create_analytics_table()
+    populate_analytics_table_task = populate_analytics_table()
+    rename_analytics_table_task = rename_analytics_table()
+
+    create_raw_table_task >> process_data_task >> load_raw_table_task >> rename_raw_table_task
+    rename_raw_table_task >> create_analytics_table_task >> populate_analytics_table_task >> rename_analytics_table_task
+
+seoul_school()
