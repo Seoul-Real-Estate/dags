19f33364ebef9e314c2e23829490ca69a871b2dd
JeonSangYong <tkddyd420@naver.com>
2024-08-26 00:03:17 +0900

    fix: 공통 모듈 추출 후 외부함수 실행

--- dags/naver_apt_real_estate.py
+++ dags/naver_apt_real_estate.py
@@ -1,10 +1,9 @@
-from datetime import datetime, timedelta
-
-import pandas as pd
-import plugins.utils.redshift_utils as redshift_utils
-import plugins.utils.aws_utils as aws_utils
-import plugins.utils.naver_utils as naver_utils
 import logging
+import pandas as pd
+from datetime import datetime, timedelta
+from utils.redshift_utils import *
+from utils.aws_utils import *
+from utils.naver_utils import *
 from airflow.decorators import task, dag, task_group
 
 SCHEMA = "raw_data"
@@ -152,26 +151,26 @@ def naver_apt_real_estate():
         # 1. 시/군/구 정보 검색 API
         @task
         def fetch_si_gun_gu():
-            if aws_utils.is_check_s3_file_exists(SI_GUN_GU_FILE_NAME):
+            if exists_s3_file(SI_GUN_GU_FILE_NAME):
                 return
 
-            df = naver_utils.get_region_info(SEOUL_CORTARNO)
-            aws_utils.upload_to_s3(SI_GUN_GU_FILE_NAME, df)
+            df = get_region_info(SEOUL_CORTARNO)
+            upload_to_s3(SI_GUN_GU_FILE_NAME, df)
 
         # 2. 읍/면/동 정보 검색 API
         @task
         def fetch_eup_myeon_dong():
-            if aws_utils.is_check_s3_file_exists(EUP_MYEON_DONG_FILE_NAME):
+            if exists_s3_file(EUP_MYEON_DONG_FILE_NAME):
                 return
 
             eup_myeon_dong_df_list = []
-            si_gun_gu_df = aws_utils.get_df_from_s3_csv(SI_GUN_GU_FILE_NAME)
+            si_gun_gu_df = get_df_from_s3_csv(SI_GUN_GU_FILE_NAME)
             for cortarNo in si_gun_gu_df['cortarNo']:
-                eup_myeon_dong_df = naver_utils.get_region_info(cortarNo)
+                eup_myeon_dong_df = get_region_info(cortarNo)
                 eup_myeon_dong_df_list.append(eup_myeon_dong_df)
 
             eup_myeon_dong_df = pd.concat(eup_myeon_dong_df_list, ignore_index=True)
-            aws_utils.upload_to_s3(EUP_MYEON_DONG_FILE_NAME, eup_myeon_dong_df)
+            upload_to_s3(EUP_MYEON_DONG_FILE_NAME, eup_myeon_dong_df)
 
         fetch_si_gun_gu() >> fetch_eup_myeon_dong()
 
@@ -183,11 +182,11 @@ def naver_apt_real_estate():
         # 3. 동마다 아파트 단지 리스트 검색
         @task.branch
         def process_apt_complex():
-            dong_df = aws_utils.get_df_from_s3_csv(EUP_MYEON_DONG_FILE_NAME)
+            dong_df = get_df_from_s3_csv(EUP_MYEON_DONG_FILE_NAME)
             complex_df_list = []
 
             for cortarNo in dong_df['cortarNo']:
-                complex_df = naver_utils.get_apt_complex(cortarNo)
+                complex_df = get_apt_complex(cortarNo)
                 complex_df_list.append(complex_df)
 
             complex_df = pd.concat(complex_df_list, ignore_index=True)
@@ -195,21 +194,21 @@ def naver_apt_real_estate():
                 columns=['detailAddress', 'totalBuildingCount', 'highFloor', 'lowFloor', 'cortarAddress', 'dealCount',
                          'leaseCount', 'rentCount', 'shortTermRentCount'], inplace=True)
 
-            complex_numbers = redshift_utils.get_primary_keys(SCHEMA, COMPLEX_NAME, "complexno")
+            complex_numbers = get_primary_keys(SCHEMA, COMPLEX_NAME, "complexno")
             new_complex_df = complex_df[~complex_df['complexNo'].isin(complex_numbers)]
 
             if new_complex_df.empty:
                 return 'fetch_and_process_apt_complex_data.skip_task'
 
             today_complex_file_name = get_today_file_name(COMPLEX_FILE_NAME)
-            aws_utils.upload_to_s3(today_complex_file_name, new_complex_df)
+            upload_to_s3(today_complex_file_name, new_complex_df)
             return 'fetch_and_process_apt_complex_data.process_apt_complex_detail'
 
         # 4. 단지마다 아파트 단지 상세정보 검색
         @task
         def process_apt_complex_detail():
             today_complex_file_name = get_today_file_name(COMPLEX_FILE_NAME)
-            complex_df = aws_utils.get_df_from_s3_csv(today_complex_file_name)
+            complex_df = get_df_from_s3_csv(today_complex_file_name)
 
             # 상세 정보에서 필요한 데이터 key
             keys = [
@@ -220,7 +219,7 @@ def naver_apt_real_estate():
 
             add_necessary_columns(complex_df, keys + ['address', 'road_address', 'created_at', 'updated_at'])
             for idx, row in complex_df.iterrows():
-                _json = naver_utils.get_apt_complex_detail(row['complexNo'])
+                _json = get_apt_complex_detail(row['complexNo'])
                 desired_json = {key: _json.get(key) for key in keys}
 
                 for key, value in desired_json.items():
@@ -243,14 +242,14 @@ def naver_apt_real_estate():
                                "managementOfficeTelNo", "roadZipCode", "road_address", "created_at", "updated_at",
                                ]
             complex_df = complex_df[desired_columns]
-            aws_utils.upload_to_s3(today_complex_file_name, complex_df)
+            upload_to_s3(today_complex_file_name, complex_df)
 
         # 5. 아파트 단지 redshift 적재
         @task
         def load_to_redshift_complex():
             today_complex_file_name = get_today_file_name(COMPLEX_FILE_NAME)
             s3_path = f"s3://team-ariel-2-data/data/{today_complex_file_name}"
-            redshift_utils.load_to_redshift_from_s3_csv(SCHEMA, COMPLEX_NAME, s3_path)
+            load_to_redshift_from_s3_csv(SCHEMA, COMPLEX_NAME, s3_path)
 
         @task
         def skip_task():
@@ -270,22 +269,22 @@ def naver_apt_real_estate():
         # 6. 아파트 단지마다 새로운 아파트 매물 리스트 검색
         @task.short_circuit(trigger_rule='one_success')
         def process_apt_real_estate():
-            complex_numbers = redshift_utils.get_primary_keys(SCHEMA, COMPLEX_NAME, "complexno")
+            complex_numbers = get_primary_keys(SCHEMA, COMPLEX_NAME, "complexno")
             real_estate_df_list = []
 
             for complexNo in complex_numbers:
-                real_estate_df = naver_utils.get_apt_real_estate(complexNo)
+                real_estate_df = get_apt_real_estate(complexNo)
                 real_estate_df['complexNo'] = complexNo
                 real_estate_df_list.append(real_estate_df)
 
             real_estate_df = pd.concat(real_estate_df_list, ignore_index=True)
-            article_numbers = redshift_utils.get_primary_keys(SCHEMA, REAL_ESTATE_NAME, "articleno")
+            article_numbers = get_primary_keys(SCHEMA, REAL_ESTATE_NAME, "articleno")
             new_real_estate_df = real_estate_df[~real_estate_df['articleNo'].isin(article_numbers)]
             if new_real_estate_df.empty:
                 return False
 
             today_real_estate_file_name = get_today_file_name(REAL_ESTATE_FILE_NAME)
-            aws_utils.upload_to_s3(today_real_estate_file_name, new_real_estate_df)
+            upload_to_s3(today_real_estate_file_name, new_real_estate_df)
             return True
 
         # 7. 매물마다 상세정보 검색 및 공인중개사 정보 생성
@@ -305,18 +304,18 @@ def naver_apt_real_estate():
             ]
             add_necessary_columns(apt_df, necessary_columns)
             for idx, row in apt_df.iterrows():
-                apt_detail_info = naver_utils.get_real_estate_detail(row['articleNo'])
+                apt_detail_info = get_real_estate_detail(row['articleNo'])
                 add_apt_detail_columns(idx, apt_df, apt_detail_info)
                 realtor = apt_detail_info.get('articleRealtor', {})
                 realtor_infos.append(realtor)
 
             today_real_estate_file_name = get_today_file_name(REAL_ESTATE_FILE_NAME)
-            aws_utils.upload_to_s3(today_real_estate_file_name, apt_df)
+            upload_to_s3(today_real_estate_file_name, apt_df)
             realtor_df = pd.DataFrame(realtor_infos)
             realtor_df = realtor_df.drop_duplicates(subset='realtorId')
             realtor_df = realtor_df[realtor_df['realtorId'].notna()]
             today_realtor_file_name = get_today_file_name(REALTOR_FILE_NAME)
-            aws_utils.upload_to_s3(today_realtor_file_name, realtor_df)
+            upload_to_s3(today_realtor_file_name, realtor_df)
 
         process_apt_real_estate() >> process_apt_real_estate_detail()
 
@@ -324,10 +323,10 @@ def naver_apt_real_estate():
     def transform_apt_real_estate():
         try:
             today_real_estate_file_name = get_today_file_name(REAL_ESTATE_FILE_NAME)
-            apt_df = aws_utils.get_df_from_s3_csv(today_real_estate_file_name)
+            apt_df = get_df_from_s3_csv(today_real_estate_file_name)
             real_estate_df = transform_apt_df(apt_df)
             today_transform_file_name = get_today_file_name('transform_' + REAL_ESTATE_FILE_NAME)
-            aws_utils.upload_to_s3(today_transform_file_name, real_estate_df)
+            upload_to_s3(today_transform_file_name, real_estate_df)
         except FileNotFoundError:
             logging.error(f"Error File '{today_real_estate_file_name}' not found in S3")
             raise
@@ -342,25 +341,25 @@ def naver_apt_real_estate():
     def load_to_redshift_real_estate():
         today_transform_file_name = get_today_file_name('transform_' + REAL_ESTATE_FILE_NAME)
         s3_path = f"s3://team-ariel-2-data/data/{today_transform_file_name}"
-        redshift_utils.load_to_redshift_from_s3_csv(SCHEMA, REAL_ESTATE_NAME, s3_path)
+        load_to_redshift_from_s3_csv(SCHEMA, REAL_ESTATE_NAME, s3_path)
 
     @task.short_circuit
     def extract_new_realtor():
         today_realtor_file_name = get_today_file_name(REALTOR_FILE_NAME)
-        realtor_df = aws_utils.get_df_from_s3_csv(today_realtor_file_name)
-        realtor_ids = redshift_utils.get_primary_keys(SCHEMA, REALTOR_NAME, "realtorid")
+        realtor_df = get_df_from_s3_csv(today_realtor_file_name)
+        realtor_ids = get_primary_keys(SCHEMA, REALTOR_NAME, "realtorid")
         new_realtor_df = realtor_df[~realtor_df['realtorId'].isin(realtor_ids)]
         if new_realtor_df.empty:
             logging.info("No new realtor data")
             return False
 
-        aws_utils.upload_to_s3(today_realtor_file_name, new_realtor_df)
+        upload_to_s3(today_realtor_file_name, new_realtor_df)
         return True
 
     @task
     def transform_realtor():
         today_realtor_file_name = get_today_file_name(REALTOR_FILE_NAME)
-        realtor_df = aws_utils.get_df_from_s3_csv(today_realtor_file_name)
+        realtor_df = get_df_from_s3_csv(today_realtor_file_name)
         desired_columns = ['realtorId', 'realtorName', 'representativeName', 'address', 'establishRegistrationNo',
                            'dealCount', 'leaseCount', 'rentCount', 'latitude', 'longitude', 'representativeTelNo',
                            'cellPhoneNo', 'cortarNo']
@@ -371,13 +370,13 @@ def naver_apt_real_estate():
         realtor_df['created_at'] = datetime.now()
         realtor_df['updated_at'] = datetime.now()
         today_transform_realtor_file_name = get_today_file_name('transform_' + REALTOR_FILE_NAME)
-        aws_utils.upload_to_s3(today_transform_realtor_file_name, realtor_df)
+        upload_to_s3(today_transform_realtor_file_name, realtor_df)
 
     @task
     def load_to_redshift_realtor():
         today_transform_realtor_file_name = get_today_file_name('transform_' + REALTOR_FILE_NAME)
         s3_path = f"s3://team-ariel-2-data/data/{today_transform_realtor_file_name}"
-        redshift_utils.load_to_redshift_from_s3_csv(SCHEMA, REALTOR_NAME, s3_path)
+        load_to_redshift_from_s3_csv(SCHEMA, REALTOR_NAME, s3_path)
 
     fetch_region = fetch_and_process_region_data()
     fetch_apt_complex = fetch_and_process_apt_complex_data()
